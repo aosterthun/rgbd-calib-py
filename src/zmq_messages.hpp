e52c8455d7af52c02e456ea7d291031f6ba88ef7
Arne Osterthun <arne.osterthun@uni-weimar.de>
2017-01-24 11:13:14 +0100

    Added more commands

--- src/zmq_messages.hpp
+++ src/zmq_messages.hpp
@@ -1,11 +1,8 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <string>
-#include <cctype>
+#define BOOL_STR(b) ((b)?"1":"0")
 
 namespace pykinecting{
     
+    //std::map<std::string,std::string> message_type = {{"PLAY","0"}, {"RECORD","1"}};
     enum Message_Type {PLAY, RECORD};
     
     std::string toString(const Message_Type& type){
@@ -46,6 +43,61 @@ namespace pykinecting{
         return msg;
     }
     
+	std::string play(Message_Type type,std::string& message_id, std::string& filepath_src, std::string& startframe, std::string& endframe){
+        std::string msg;
+		std::string exType = pykinecting::toString(type);
+		exType.append(2 - exType.length(), '_');
+        filepath_src.append(255 - filepath_src.length(), '_');
+        message_id.append(3 - message_id.length(), '_');
+		startframe.append(5 - startframe.length(), '_');
+		endframe.append(5 - endframe.length(), '_');
+        msg.append(exType);
+        msg.append(message_id);
+        msg.append(filepath_src);
+		msg.append(startframe);
+		msg.append(endframe);
+        return msg;
+    }
+	
+	std::string play(Message_Type type,std::string& message_id, std::string& filepath_src, std::string& startframe, std::string& endframe, bool loop){
+		std::string msg;
+		std::string exType = pykinecting::toString(type);
+		exType.append(2 - exType.length(), '_');
+		filepath_src.append(255 - filepath_src.length(), '_');
+		message_id.append(3 - message_id.length(), '_');
+		startframe.append(5 - startframe.length(), '_');
+		endframe.append(5 - endframe.length(), '_');
+		msg.append(exType);
+		msg.append(message_id);
+		msg.append(filepath_src);
+		msg.append(startframe);
+		msg.append(endframe);
+		msg.append(BOOL_STR(loop));
+		
+		return msg;
+	}
+
+	std::string record_play(Message_Type type,std::string& message_id, std::string& filepath_src, std::string& startframe, std::string& endframe, std::string& filepath_dest, std::string& num_cameras){
+		std::string msg;
+		std::string exType = pykinecting::toString(type);
+		exType.append(2 - exType.length(), '_');
+		filepath_src.append(255 - filepath_src.length(), '_');
+		filepath_dest.append(255 - filepath_dest.length(), '_');
+		message_id.append(3 - message_id.length(), '_');
+		startframe.append(5 - startframe.length(), '_');
+		endframe.append(5 - endframe.length(), '_');
+		msg.append(exType);
+		msg.append(message_id);
+		msg.append(filepath_src);
+		msg.append(startframe);
+		msg.append(endframe);
+		msg.append(filepath_dest);
+		msg.append(num_cameras);
+		
+		return msg;
+	}
+	
+	
     std::string expandToLength(size_t toExpand, int length){
         std::string tmp_str = std::to_string(toExpand);
         tmp_str.append(length - tmp_str.length(), '\t');
@@ -53,4 +105,3 @@ namespace pykinecting{
     }
 
 }
-
