0d6c6aa98e5321c72e0ed5be024120c85784b430
Arne Osterthun <arne.osterthun@uni-weimar.de>
2018-02-05 12:21:29 +0100

    Example uses kinect recordings in /opt/kinect-resources/kinect_daemon/kinect_recordings/

--- examples/main.py
+++ examples/main.py
@@ -4,7 +4,7 @@ import pyrgbdcalib
 
 if __name__ == '__main__':
     daemon = pyrgbdcalib.KinectDaemon("141.54.147.108","141.54.147.106")
-    recording = daemon.record("/home/mejo6715/Hiwi/kinect_recordings/rgbdri_rec_2.stream","141.54.147.106:7000",4)
+    recording = daemon.record("/opt/kinect_resources/kinect_daemon/kinect_recordings/rgbdri_rec_2.stream","141.54.147.106:7000",4)
     time.sleep(5)
     recording.stop()
 
