adac65e2fad54b882a468cd2eab5e5207afc01c7
Arne Osterthun <arne.osterthun@uni-weimar.de>
2018-02-05 13:35:16 +0100

    Modified main.py

--- examples/main.py
+++ examples/main.py
@@ -4,13 +4,13 @@ import pyrgbdcalib
 
 if __name__ == '__main__':
     daemon = pyrgbdcalib.KinectDaemon("141.54.147.108","141.54.147.106")
-    recording = daemon.record("/opt/kinect_resources/rgbd-framework/rgbd-daemon/kinect_recordings/rgbdri_rec_2.stream","141.54.147.106:7000",4)
+    recording = daemon.record("/opt/kinect-resources/rgbd-framework/rgbd-daemon/kinect_recordings/rgbdri_rec_3.stream","141.54.147.108:7000",4)
     time.sleep(5)
     recording.stop()
 
     time.sleep(5)
 
-    play_recording = daemon.play(recording.filename(),"141.54.147.108:7000",4)
+    play_recording = daemon.play(recording.filename(),"141.54.147.108:7001",4)
 
 
     while True:
